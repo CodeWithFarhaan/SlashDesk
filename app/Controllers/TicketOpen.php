d1eb139e6f3ea00013fbf0ecd305c2212b60fe0f
Farhaan Chaudhary <farhaanchaudhary777@gmail.com>
2025-07-23 03:56:46 +0530

    debugging...

--- app/Controllers/TicketOpen.php
+++ app/Controllers/TicketOpen.php
@@ -6,6 +6,49 @@ class TicketOpen extends BaseController
 {
     public function index()
     {
-        return view('pages/ticketOpen'); 
+        if (!session()->get('isLoggedIn') || !session()->get('token')) {
+                return redirect()->to('login');
+            }
+            $client = \Config\Services::curlrequest();
+            $url = env("URL_BACKEND")."/ticket/tickets";
+
+            try{
+                $response = $client->request('GET', $url, [
+                        'headers' => [
+                            'Accept' => 'application/json',
+                            'Authorization' => 'Bearer '.substr(session()->get('token'),0,) 
+                        ],
+                        'json'=> [
+                            'pageState' => null,
+                            'pageSize' => "10"
+                        ],  
+                        'http_errors' => false // To handle HTTP errors manually
+                ]);
+                $responseBody = json_decode($response->getBody(), true);
+                $statusCode = $response->getStatusCode();
+              
+                if ($statusCode >= 200 && $statusCode < 300){
+                    // print_r($responseBody);
+                    // die;
+                    $data["responseBody"] = $responseBody;
+
+                    return view('pages/ticketOpen',$data);
+                }else{
+                    // print_r(substr(session()->get('token'),0,));        
+                    print_r($responseBody);
+                    print_r($statusCode);
+                }
+            }catch(\Exception $e){
+                log_message('error', 'Registration exception: ' . $e->getMessage());
+                // print_r(esc(session()->get('userData')['data']['token']));
+                return $this->response->setJSON([
+                    'status' => false,
+                    'message' => $e->getMessage()
+                ])->setStatusCode(500);
+            }
+            // view('pages/myTicket'); 
+    }
+    public function getAllTicket(){
+
     }
 }
